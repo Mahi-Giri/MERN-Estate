551965e1d34b9deac84f29e2bc92b7977283a457
Mahesh Giri <girimahesh.m@gmail.com>
2024-05-02 22:27:18 +0530

    Create Pages folder and add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
 function App() {
     return (
-        <div className="">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
