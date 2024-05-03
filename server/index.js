e7ba94aeea65d93cfa08e4d967caf65e98d20efd
Mahesh Giri <girimahesh.m@gmail.com>
2024-05-03 22:23:33 +0530

    Optimized code like production build

--- server/index.js
+++ server/index.js
@@ -1,24 +1,15 @@
-import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-const app = express();
-
-mongoose
-    .connect(process.env.MONGO_CONNECTION_STRING)
+connectDB()
     .then(() => {
-        console.log("Connected to Mongoose server");
+        app.listen(5000, () => {
+            console.log("Server is running on port 5000");
+        });
     })
     .catch((err) => {
         console.log(`Unable to connect mongoDB server ${err}`);
     });
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
